a95592fb9c9131c0240b58cad1d32150771e759e
Gregory Loden <gregory.loden@gmail.com>
2020-02-01 03:45:39 -0500

    restrict the player to only jump when touching the ground

---
+++ Assets/Scripts/GroundDetecter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetecter: MonoBehaviour {
+	private int groundLayerMask;
+	public bool onGround { get; private set; } = false;
+
+	// Start is called before the first frame update
+	void Start() {
+		groundLayerMask = LayerMask.NameToLayer("Ground");
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		if(otherCollider.gameObject.layer == groundLayerMask)
+			onGround = true;
+	}
+
+	void OnTriggerExit2D(Collider2D otherCollider) {
+		if(otherCollider.gameObject.layer == groundLayerMask)
+			onGround = false;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController: MonoBehaviour {
 	public float baseVelocity;
 	public float jumpPower;
+	public GroundDetecter groundDetecter;
+
 	private new Rigidbody2D rigidbody2D;
 
 	// Start is called before the first frame update
@@ -15,7 +17,7 @@ public class PlayerController: MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * baseVelocity, rigidbody2D.velocity.y);
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown("Jump") && groundDetecter.onGround)
 			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
 		if (rigidbody2D.velocity.x != 0)
 			transform.localScale = new Vector3(rigidbody2D.velocity.x < 0 ? -1 : 1, transform.localScale.y, 1);
