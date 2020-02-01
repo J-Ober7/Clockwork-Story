ba926e57e283020fe4125959d8eb23d822694ea8
Gregory Loden <gregory.loden@gmail.com>
2020-02-01 01:21:58 -0500

    add player movement and jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour {
+	public float baseVelocity;
+	private new Rigidbody2D rigidbody2D;
+
+	// Start is called before the first frame update
+	void Start() {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * baseVelocity, rigidbody2D.velocity.y);
+		if (Input.GetButtonDown("Jump"))
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, baseVelocity);
+	}
+}
