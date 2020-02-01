5268757f93fd9d06ade9381dcd67b921babdab55
Gregory Loden <gregory.loden@gmail.com>
2020-02-01 13:27:18 -0500

    make the cog a physical object, picking it up and putting it down sticks it to the player

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteract: MonoBehaviour {
-    private bool hasCog;
-    private GameObject cogHeld;
     public GameObject pickUp;
+
+    private GameObject cogHeld = null;
+    private Transform oldCogHeldTransform = null;
 
     // Start is called before the first frame update
     void Start() {
-        hasCog = false;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetButtonDown("Grab")) {
-            if (hasCog) {
-                cogHeld.transform.position = new Vector3(transform.position.x, transform.position.y, cogHeld.transform.position.z);
-                cogHeld.SetActive(true);
-                hasCog = false;
+            if (cogHeld != null) {
+                cogHeld.transform.parent = oldCogHeldTransform;
+                cogHeld = null;
             } else if (pickUp != null) {
-                hasCog = true;
                 cogHeld = pickUp;
-                cogHeld.SetActive(false);
+                oldCogHeldTransform = cogHeld.transform.parent;
+                cogHeld.transform.parent = transform;
                 pickUp = null;
             }
         }
